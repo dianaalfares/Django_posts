75e2ae3ea071e486aae4f31499ca246ab0962b17
diana alfares <alfaresdiana78@gmail.com>
2025-11-03 23:58:41 +0200

    5

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -62,9 +62,8 @@ def register(request):
      logger.info("the user create successfully")
      return Response({"result":"ok","token":us.token})
    except Exception as e:
-      print(e)
-      logger.error(f'something happend {e}')
-      raise ValueError(f"Error : {e}")
+      # logger.error(f'something happend {e}')
+      raise ValueError(f"Error :{e}")
    
 
 
@@ -74,17 +73,27 @@ def register(request):
 @api_view(['POST'])
 def Login(request):
    try:
+     logger.info("start order to log in")
      passw=request.data.get('password')
      user_name=request.data.get('user_name')
+     if len(passw)<8 :
+        logger.warning('you should add password with 8 character or more..')
+        raise ValueError("you must add a password with 8 character or more..")
+     if  not  User.objects.filter(username=user_name):
+        logger.error('user name doesnot exist')
+        raise ValueError("user name doesnot exist")
      user=authenticate(username=user_name,password=passw)
+     if not user :
+       logger.error('wrong password')
+       raise ValueError("wrong password")
      token=get_tokens_for_user(user)
      prof=profile.objects.get(user=user)
      prof.token=token['access']
      prof.save()
+     logger.info('Log in is ok')
      return Response({"result":"ok","token":prof.token})
    except Exception as e:
-      print(e)
-      return Response({"result" : "error"})    
+      raise ValueError(f"Error : {e}") 
    
 
 
@@ -93,13 +102,14 @@ def Login(request):
 @api_view(['POST'])
 def get_profile(request):
    try:
+    logger.info("start order to get profile")
     token=request.data.get('token')
     prof=profile.objects.get(token=token)
     details=Profileserializer(prof)
+    logger.info("all data are prepeared")
     return Response({"result":"ok","details":details.data})
    except Exception as e:
-      print(e)
-      return Response({"result" : "error"})   
+       raise ValueError(f"Error :{e}")  
 
 
 
@@ -108,13 +118,13 @@ def get_profile(request):
 @api_view(['GET'])
 def read_posts(request):
    try:
+    logger.info("start order to read all posts")
     allposts=post.objects.all()
     serializerposts = PostSerializer(allposts, many=True)
-  
+    logger.info("every thing ok")
     return Response({"result":"ok","data":serializerposts.data})
    except Exception as e:
-      print(e)
-      return Response({"result" : "error"}) 
+     raise ValueError(f"Error :{e}")   
    
 
 
@@ -122,40 +132,49 @@ def read_posts(request):
 @api_view(['POST'])
 def create_post(request):
    try:
+    logger.info("start order to create post")
     token=request.data.get('token')
     text=request.data.get('text')
     image=request.FILES.get('image')
-    print('hello')
     prof=profile.objects.get(token=token)
-  
-    post_for_user=post(u=prof,text=text)
+    post_for_user=post(u=prof,text=text,image=image)
     post_for_user.save()
-    post_for_user.image=image
-    post_for_user.save()
+    logger.info("every thing ok")
     return Response({"result":"ok","id":post_for_user.id})
    except Exception as e:
-      print(e)
-      return Response({"result" : "error"})    
+    raise ValueError(f"Error :{e}")       
    
 
 
 
 
-@api_view(['POST'])
-def update_post(request):
-   try:
-    id=request.data.get('id')
-    text=request.data.get('text')
-    image=request.FILES.get('image')
-    print('hello')
-    post_for_user=post.objects.get(id=id)
-    post_for_user.text=text
-    post_for_user.image=image
-    post_for_user.save()
-    return Response({"result":"ok"})
-   except Exception as e:
-      print(e)
-      return Response({"result" : "error"})    
+# @api_view(['POST'])
+# def update_post(request):
+   # try:
+   #  logger.info("start order to update post")
+   #  id=request.data.get('id')
+   #  token=request.data.get('token')
+   #  text=request.data.get('text')
+   #  image=request.FILES.get('image')
+   #  if  not profile.objects.get(token=token):
+      #  logger.error("token is not correct")
+      #  raise ValueError("token is not correct") 
+   #  prof=profile.objects.get(token =token)
+   #  if  not post.objects.get(u=prof):
+      #  logger.error("post does not belong to this user")
+      #  raise ValueError("post does not belong to this user") 
+   #  if  not post.objects.get(id=id):
+      #  logger.error("id does not correct")
+      #  raise ValueError("id does not correct") 
+   #  post_for_user=post.objects.get(id=id)
+   #  post_for_user.text=text
+   #  post_for_user.image=image
+   #  post_for_user.save()
+   #  logger.info("every thing ok")
+   #  return Response({"result":"ok"})
+   # except Exception as e:
+   #   raise ValueError(f"Error :{e}")       
+       
    
 
 
